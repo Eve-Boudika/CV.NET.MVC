cc9b07545e710de67e4a945b3619c9e961305293
Evelyn <evelabohemia@gmail.com>
2023-07-19 20:38:30 -0300

    cambios en index html

--- WebCv/Controllers/HomeController.cs
+++ WebCv/Controllers/HomeController.cs
@@ -23,6 +23,16 @@ namespace WebCv.Controllers
             return View();
         }
 
+        public IActionResult ExperienciaLaboral()
+        {
+            return View();
+        }
+
+        public IActionResult Educacion()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
