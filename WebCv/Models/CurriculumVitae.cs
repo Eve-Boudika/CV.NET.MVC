5f3494d359b7f49638a91a4c455849b09c054274
Evelyn <evelabohemia@gmail.com>
2023-07-18 22:09:15 -0300

    instalación bootstrap Creación de vista modelo y controller

---
+++ WebCv/Models/CurriculumVitae.cs
@@ -0,0 +1,11 @@
+﻿namespace WebCv.Models
+{
+    public class CurriculumVitae
+    {
+        public string Nombre { get; set; }
+        public string ExperienciaLaboral { get; set; }
+        public string Educacion { get; set; }
+        public List<string> Habilidades { get; set; }
+
+    }
+}
