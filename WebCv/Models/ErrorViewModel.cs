074f007e39e82bed02df3e290d365329ed15675d
Evelyn <evelabohemia@gmail.com>
2023-07-18 21:34:04 -0300

    Subida primigenia

---
+++ WebCv/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebCv.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
